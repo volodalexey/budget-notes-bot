7c980531d647e53eec22145b1b2fd9beb7314f65
Alexey V <volodalexey@gmail.com>
2022-12-08 12:03:00 +0300

    Test ssh connection with CI

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,19 +9,19 @@ module.exports = {
   ],
   deploy: {
     production: {
-      key: process.env.SSH_KEY_PATH,
       user: process.env.SSH_USER,
       host: process.env.SSH_HOST,
       port: process.env.SSH_PORT,
       ssh_options: 'StrictHostKeyChecking=no',
-      ref: 'origin/master',
-      repo: 'git@gitlab.com:volodalexey/budget-notes-bot.git',
-      path: '/home/vl/budget-notes-bot',
+      ref: 'origin/main',
+      repo: 'git@github.com:volodalexey/budget-notes-bot.git',
+      path: process.env.SSH_CWD,
       'post-deploy': [
         process.env.PGPASSWORD
           ? `PGPASSWORD=${process.env.PGPASSWORD} pg_dump --username=${process.env.SSH_USER} --no-owner memory > ~/memory-backups/memory_$(date +%d_%m_%y).bak`
           : false,
         `npm ci`,
+        `npm run build`,
         `pm2 startOrRestart ecosystem.config.js --env production`,
       ]
         .filter(Boolean)
